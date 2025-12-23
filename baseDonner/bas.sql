873043fe8505635752c54973df97e5280c32f219
ajartijaouad <ajartijaouad14@gmail.com>
2025-12-23 16:30:08 +0100

    ajouter la table projets a la basedonées

--- baseDonner/bas.sql
+++ baseDonner/bas.sql
@@ -5,3 +5,13 @@ create table membres (
     email varchar(100) not null UNIQUE ,
     date_inscription date not null 
 );
+create table  projets (
+    id int AUTO_INCREMENT primary key ,
+    titre varchar(150) not null,
+    date_debut DATE not null,
+    statut varchar(50) not null,
+    membre_id INT not null,
+    type varchar(30) not null,
+
+    foreign key(membre_id)  references membres(id)
+);
