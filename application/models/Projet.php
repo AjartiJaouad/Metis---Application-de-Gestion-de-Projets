1fb52589fa2212e7312f24be92b98e821bdf749f
ajartijaouad <ajartijaouad14@gmail.com>
2025-12-25 20:28:03 +0100

    creation de la class abstraite Projet avec méthode polymorphe

--- application/models/Projet.php
+++ application/models/Projet.php
@@ -1,9 +1,17 @@
 <?php
+
 namespace App\Models;
 
 use App\Noyau\BaseModel;
 
-class Projet extends BaseModel
+abstract class Projet extends BaseModel
 {
     protected string $table = 'projets';
+
+    protected ?int $id = null;
+    protected string $titre;
+    protected string $description;
+
+    // Methode abstraite polymorphisme
+    abstract public function get_duree(): string;
 }
--- public/index.php
+++ public/index.php
@@ -21,3 +21,26 @@
 // $membre->sauvegarder();
 
 // echo "Membre créé ";
+
+
+// require_once __DIR__ . '/../application/Database/Database.php';
+// require_once __DIR__ . '/../application/Noyau/BaseModel.php';
+// require_once __DIR__ . '/../application/Models/Membre.php';
+
+// use App\Models\Membre;
+
+// try {
+//     $membre = new Membre();
+
+//     $membre->set_nom("Anas");
+//     $membre->set_email("anas@test.com");
+//     $membre->set_date_inscription(date('Y-m-d'));
+
+//     echo "✅ Email valide et unique <br>";
+//     echo "Nom : " . $membre->get_nom() . "<br>";
+//     echo "Email : " . $membre->get_email() . "<br>";
+//     echo "Date : " . $membre->get_date_inscription();
+
+// } catch (Exception $e) {
+//     echo "❌ Erreur : " . $e->getMessage();
+// }
