002683c8ba8db1104698ec0f4f95d820a81b2b0b
ajartijaouad <ajartijaouad14@gmail.com>
2025-12-25 12:06:01 +0100

    ajoouter de la classe membre avec propritées privaeés id nom email data

--- application/models/membre.php
+++ application/models/membre.php
@@ -2,8 +2,13 @@
 namespace App\Models;
 
 use App\Noyau\BaseModel;
-
+use PDO;
+use Exception;
 class Membre extends BaseModel
 {
     protected string $table = 'membres';
+    private ? int $id = null ;
+    private string $nom;
+    private string $email ;
+    private string $data_inscription ;
 }
