a7228e44e1840eab8e8cf3b66926265b67cf29a2
ajartijaouad <ajartijaouad14@gmail.com>
2025-12-25 14:28:20 +0100

    feat :ajout des getters et setters de mombre

--- application/models/membre.php
+++ application/models/membre.php
@@ -1,14 +1,51 @@
 <?php
+
 namespace App\Models;
 
 use App\Noyau\BaseModel;
 use PDO;
 use Exception;
+
 class Membre extends BaseModel
 {
+
     protected string $table = 'membres';
-    private ? int $id = null ;
+
+
+    private ?int $id = null;
     private string $nom;
-    private string $email ;
-    private string $data_inscription ;
+    private string $email;
+    private string $date_inscription;
+
+    public function get_id(): ?int
+    {
+        return $this->id;
+    }
+
+    public function get_nom(): string
+    {
+        return $this->nom;
+    }
+
+    public function set_nom(string $nom)
+    {
+        $this->nom = trim($nom);
+    }
+    public function get_email(): string
+    {
+        return $this->email;
+    }
+    public function set_email(string  $email)
+    {
+        $this->email = trim($email);
+    }
+    public function get_date_inscription(): string
+    {
+        return $this->date_inscription;
+    }
+    public function set_date_inscription(string $date)
+    {
+        return
+            $this->date_inscription = $date;
+    }
 }
