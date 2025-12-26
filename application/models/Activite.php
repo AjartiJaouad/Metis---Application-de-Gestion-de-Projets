8f7c811ebd5772248e6e629134e54969904919f2
ajartijaouad <ajartijaouad14@gmail.com>
2025-12-26 10:04:38 +0100

    ajouter de la methode d'ajout d une activite

--- application/models/Activite.php
+++ application/models/Activite.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use App\Noyau\BaseModel;
@@ -6,8 +7,25 @@ use App\Noyau\BaseModel;
 class Activite extends BaseModel
 {
     protected string $table = 'activites';
-    protected ?int  $id = null ;
-    protected int $projet_id ;
-    protected string $description ;
-    protected string $date_activite ;
+
+    protected ?int $id = null;
+    protected int $projet_id;
+    protected string $description;
+    protected string $date_activite;
+    protected string $statut;
+
+    public function ajouterActivite(int $projet_id, string $description, string $statut)
+    {
+        $sql = "INSERT INTO activites (description, dateActivite, statut, projets_id)
+                VALUES (:description, :dateActivite, :statut, :projets_id)";
+
+        $stmt = $this->db->prepare($sql);
+
+        return $stmt->execute([
+            'description'  => $description,
+            'dateActivite' => date('Y-m-d'),
+            'statut'       => $statut,
+            'projets_id'   => $projet_id
+        ]);
+    }
 }
