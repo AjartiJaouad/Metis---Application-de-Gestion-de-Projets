cce3faa00d0089b04e97d1772643c5a48d1aa570
ajartijaouad <ajartijaouad14@gmail.com>
2025-12-26 09:53:06 +0100

    creation de la class Activite

--- application/models/Activite.php
+++ application/models/Activite.php
@@ -6,4 +6,8 @@ use App\Noyau\BaseModel;
 class Activite extends BaseModel
 {
     protected string $table = 'activites';
+    protected ?int  $id = null ;
+    protected int $projet_id ;
+    protected string $description ;
+    protected string $date_activite ;
 }
